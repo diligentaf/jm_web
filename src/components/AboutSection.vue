d822df60d16242341dc3504923919dae32249485
moog.kwon <diligentaf@gmail.com>
2022-09-23 17:17:45 +0900

    updated cards

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -5,12 +5,12 @@
         <v-col cols="10">
           <v-row align="center" justify="center">
             <v-col cols="12" md="7">
-              <h1 class="font-weight-light display-2">ietwork</h1>
-              <h1 class="font-weight-light display-1 mb-3">
-                Lorem ipsum dolor!
+              <h1 class="font-weight-light display-2 white--text">What we stand for</h1>
+              <h1 class="font-weight-light display-1 mb-3 white--text">
+                <!-- JM Node promises you these 3 things 👇 -->
               </h1>
               <v-row>
-                <v-col cols="12" class="d-flex align-center">
+                <v-col cols="12" class="d-flex align-center white--text">
                   <v-img
                     src="@/assets/img/icon1.svg"
                     max-width="60px"
@@ -20,37 +20,33 @@
                     Secure the Internet of Blockchains and earn rewards
                   </p>
                 </v-col>
-                <v-col cols="12" class="d-flex align-center">
+                <v-col cols="12" class="d-flex align-center white--text">
                   <v-img
                     src="@/assets/img/icon2.svg"
                     max-width="60px"
                     class="mr-4"
                   />
                   <p class="text-justify">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Real-time community engagement
                   </p>
                 </v-col>
-                <v-col cols="12" class="d-flex align-center">
+                <v-col cols="12" class="d-flex align-center white--text">
                   <v-img
                     src="@/assets/img/icon3.svg"
                     max-width="60px"
                     class="mr-4"
                   />
                   <p class="text-justify">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Transparent reward distribution
                   </p>
                 </v-col>
               </v-row>
             </v-col>
             <v-col cols="12" md="5" class="d-none d-md-flex">
               <v-img
-                src="@/assets/img/role.gif"
+                src="@/assets/img/a.gif"
                 class="d-block ml-auto mr-auto"
-                max-width="200px"
+                max-width="1000px"
               />
             </v-col>
           </v-row>
@@ -62,8 +58,6 @@
 
 <style scoped>
 #about {
-  /* background-color: #f4f7f5; */
-  /* background-color: transparent; */
-  background-color: rgba(255, 255, 255, 0.8);
+   background-color: rgba(0, 0, 0, 0.8) !important;
 }
 </style>
